c99f4ab6a8b9eaec468a6911f9223641823ff4af
root <Oluwayemisi>
2022-06-14 13:14:19 -0700

    task 11

---
+++ shell_concept/11-stat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/**
+ * main - stat example
+ *
+ * Return: Always 0.
+ */
+void _stat(int ac, char **av)
+{
+    unsigned int i;
+    struct stat st;
+
+    if (ac < 2)
+    {
+        printf("Usage: %s path_to_file ...\n", av[0]);
+        return;
+    }
+    i = 1;
+    while (av[i])
+    {
+        printf("%s:", av[i]);
+        if (stat(av[i], &st) == 0)
+        {
+            printf(" FOUND\n");
+        }
+        else
+        {
+            printf(" NOT FOUND\n");
+        }
+        i++;
+    }
+    return;
+}
+
+int main(int ac, char **av)
+{
+	_stat(ac, av);
+
+	return (0);
+
+}
