d38155a44e3e12483eb6d4f8d6a259cf77c2ebfb
Hemjay1ne <sulaiomonmojeed14@gmail.com>
2022-06-14 13:08:52 -0700

    environ.c

---
+++ shell_concept/13-env_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the environment
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av, char **env)
+{
+    unsigned int i;
+	
+    i = 0;
+    (void)ac;
+    (void)av;
+    while (env[i] != NULL)
+    {
+        printf("%s\n", env[i]);
+        i++;
+    }
+    return (0);
+}
